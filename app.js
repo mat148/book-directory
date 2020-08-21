4fcb73c021a902da81b28acd776f5d19da61d25d
Mathew Morse <mrm1779@gmail.com>
2020-08-20 23:10:22 -0400

    Added search input and page routing.

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const express = require('express');
+const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 require('body-parser-xml')(bodyParser);
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || "3000";
 
+app.use('/public', express.static(path.join(__dirname, '../static')));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.xml());
 
 //HOME
 app.get('/', (req, res)=> {
-    res.send("Hello World");
+    res.render('homepage');
 });
 
 //SEARCH
 app.get('/search', (req, res)=>{
-    res.send("Search");
-    getQuery(req.query.query);
+    res.render('search');
+    //getQuery(req.query.query);
+});
+
+app.post('/search', (req, res)=>{
+    const searchQuery = req.body.searchQuery;
+    //console.log(searchQuery);
+    res.render('search', {data : { searchQuery : searchQuery }});
+    getQuery(searchQuery);
+    res.end();
 });
 
 app.listen(port, ()=>{
@@ -25,7 +38,8 @@ app.listen(port, ()=>{
 });
 
 function getQuery(query) {
-    console.log(query);
+    //console.log(query);
+    console.log( process.env.goodreadskey );
     /*axios.get('').then(response => {
         console.log(response.data);
     }).catch(error => {
