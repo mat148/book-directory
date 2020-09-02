12fa32e8a7a4f50782cfa918c3524d768c63fe5e
Mathew Morse <mrm1779@gmail.com>
2020-09-02 12:10:57 -0400

    Clean up and uninstall packages that aren't needed

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const convert = require('xml-js');
 const xml2js = require('xml2js');
-//require('body-parser-xml')(bodyParser);
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -27,25 +25,13 @@ app.get('/', (req, res)=> {
 app.get('/search', (req, res)=>{
     res.render('search', {data : { goodReadsResponse : 'text', searchQuery : 'searchQuery' }});
     res.end();
-    //getQuery(req.query.query);
 });
 
 app.post('/search', (req, res)=>{
     const searchQuery = req.body.searchQuery;
     const cleanQuery = encodeURI(searchQuery);
-    //const goodReadsXML = getQuery(searchQuery);
-    //res.sendFile(goodReadsXML);
 
     axios.get( goodreadsapi + '?key=' + goodreadskey + '&q=' + cleanQuery ).then(response => {
-
-        /*var result = convert.xml2js(response.data, {compact: false, spaces: 4});
-        let temp = result.elements.map( e => {
-            return e.elements.filter( f => {
-                return f.name == 'Request';
-            });
-        });
-        console.log(temp[0][0].elements);*/
-
         xml2js.parseString(response.data, (error, result)=> {
             var test = result.GoodreadsResponse.search[0].results[0].work.map( w => {
                 let bestBook = w.best_book[0];
@@ -59,40 +45,8 @@ app.post('/search', (req, res)=>{
     }).catch(error => {
         console.log(error);
     });
-    //console.log(getQuery(searchQuery));
-    //console.log(searchQuery);
-    //console.log(getQuery(searchQuery));
-    //console.log(req.body);
 });
-
-/*axios.get( goodreadsapi + '?key=' + goodreadskey + '&q=' + cleanQuery ).then(response => {
-    //console.log(response.data);
-    //Parse XML data?
-    //console.log(response.xml());
-    return response.data
-}).catch(error => {
-    console.log(error);
-});*/
-
-/*axios.post('/search').then((res) => {
-    console.log(res);
-});*/
 
 app.listen(port, ()=>{
     console.log(`Listening to requests on http://localhost:${port}`);
 });
-
-/*const getQuery = function(query) {
-    //console.log(query);
-    //console.log( process.env.goodreadskey );
-    //https://www.goodreads.com/search/index.xml?key=b0L0YkqXxrPPmyhgKSJhLw&q=Test
-    const cleanQuery = encodeURI(query);
-    return axios.get( goodreadsapi + '?key=' + goodreadskey + '&q=' + cleanQuery ).then(response => {
-        //console.log(response.data);
-        //Parse XML data?
-        //console.log(response.xml());
-        return response.data
-    }).catch(error => {
-        console.log(error);
-    });
-}*/
