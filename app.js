e9856f116bae090a6d806e46a90f123d9c3f5a0d
Mathew Morse <mrm1779@gmail.com>
2020-09-03 00:04:37 -0400

    Parse out all data and display it plainly on the page

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.get('/', (req, res)=> {
     res.render('homepage');
 });
 
+let totalResults = 0;
+let resultsEnd = 0;
+let numberOfPaginations = 0;
+
 //Search
 app.get('/search', async (req, res) => {
     const query = req.query.searchQuery;
@@ -33,12 +37,24 @@ app.get('/search', async (req, res) => {
             const xml = await response.text();
 
             xml2js.parseString(xml, (error, result) => {
+
+                var paginations = result.GoodreadsResponse.search[0];
+                totalResults = paginations['total-results'];
+                resultsEnd = paginations['results-end'];
+                numberOfPaginations = totalResults / resultsEnd;
+
                 var test = result.GoodreadsResponse.search[0].results[0].work.map( w => {
                     let bestBook = w.best_book[0];
-                    return {title: bestBook.title[0]}
+                    return {
+                        title: bestBook.title[0],
+                        author: bestBook.author[0].name,
+                        image_url: bestBook.image_url[0],
+                        small_image: bestBook.small_image_url[0],
+                        original_publication_year: w.original_publication_year[0]._,
+                    }
                 });
 
-                res.render('search', {data : { goodReadsResponse : test, searchQuery : query }});
+                res.render('search', {data : { goodReadsResponse : test, searchQuery : query}});
                 res.end();
             });
         } catch (error) {
@@ -46,7 +62,7 @@ app.get('/search', async (req, res) => {
         }
     };
 
-    fetch_response(goodreadsapi + '?key=' + goodreadskey + '&q=' + query + '&page=1', query);
+    fetch_response(goodreadsapi + '?key=' + goodreadskey + '&q=' + query + '&page=0', query);
 });
 
 app.listen(port, ()=>{
